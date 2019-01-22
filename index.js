f5d0c7efe3a9795a1fc652fd0e8bfeb136b335af
王焱辉 <wyh_19@163.com>
2019-01-22 13:42:32 +0800

    开发

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let express = require('express')
-let app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
 app.get('/',(req,res)=>{
     res.end('hello')
