6f9f3f03706fc327587305cdf5512a940decc3e9
王焱辉 <wyh_19@163.com>
2019-01-23 20:19:54 +0800

    开发

--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,19 @@ const models = require('../models')
 const User = models.getModel('user')
 const _filter = { pwd: 0, __v: 0 }
 
+/**
+ * 获取列表
+ */
 Router.get('/list', (req, res) => {
     User.find({}, _filter, (err, doc) => {
         return res.json({ code: 0, data: doc })
     })
+})
+
+
+Router.post('/create',(req,res) => {
+    const {name,pwd} = req.body
+    
 })
 
 module.exports = Router
