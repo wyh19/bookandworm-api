b2256c53a45d5614de209ad12f6263f4a9cf95de
王焱辉 <wyh_19@163.com>
2019-03-22 11:33:54 +0800

    修改错误

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const _filter = { pwd: 0, __v: 0 }
 Router.post('/login', (req, res) => {
     //暂时要求前端传的是明文
     const { name, pwd } = req.body
-    User.findOne({ name, md5Pwd(pwd) }, _filter,(err,doc)=>{
+    User.findOne({ name, pwd:md5Pwd(pwd) }, _filter,(err,doc)=>{
         if(!doc){
             return res.json({code:1,msg:'用户名或密码错误'})
         }
