b045fb7b47cde1dd2638b64907966614725964c2
wyh19 <wyh_19@163.com>
2019-08-14 15:40:12 +0800

    添加中间件

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const app = express()
 const userRouter = require('./routes/user')
 const authRouter = require('./routes/auth')
 const bookRouter = require('./routes/book')
 
+const app = express()
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+app.use(morgan('combined', { stream: accessLogStream }))
 app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use('/auth',authRouter)
-app.use('/user',userRouter)
-app.use('/book',bookRouter)
+
+app.use('/auth', authRouter)
+app.use('/user', userRouter)
+app.use('/book', bookRouter)
 
 
 app.listen(3000)
